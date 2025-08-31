3a3076dbe5fb789249aa57f5500c503dfb968735
vikasyadav5744 <vikram.kumar500@gmail.com>
2025-08-31 11:13:05 +0530

    Update niftyfinal.py

--- niftyfinal.py
+++ niftyfinal.py
@@ -5,7 +5,8 @@ header = st.header('Nifty View')
 file = st.file_uploader(' please select your csv file' )
 data = pd.read_csv(file, skiprows=1)
 st.write(data)
-st.write(data.columns)
+st.write(data.columns.pop([0,23])
+
 
 
 
