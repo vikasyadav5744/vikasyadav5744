9e970bf0b50187317679be6b7dd394d593c3787d
vikasyadav5744 <vikram.kumar500@gmail.com>
2025-08-31 10:53:04 +0530

    Update niftyfinal.py

--- niftyfinal.py
+++ niftyfinal.py
@@ -3,9 +3,10 @@ import streamlit as st
 import numpy as np
 header = st.header('Nifty View')
 file = st.file_uploader(' please select your csv file' )
-data = pd.read_csv(file, skiprows=1, fillna=0)
+data = pd.read_csv(file, skiprows=1)
 st.write(data.columns)
 st.write(data)
+
 
 
 
