af1294ad30a71dd60c2d543b2031b6da585a26ff
vikasyadav5744 <vikram.kumar500@gmail.com>
2025-09-01 20:44:58 +0530

    Update niftyfinal.py

--- niftyfinal.py
+++ niftyfinal.py
@@ -2,8 +2,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import time
- 
+from datetime import time 
 st.set_page_config(page_title=None, page_icon=None, layout="wide", initial_sidebar_state="auto", menu_items=None) 
 
 pd.options.mode.copy_on_write = True
@@ -640,6 +639,7 @@ with tab7:
 
 
        
+
 
 
 
