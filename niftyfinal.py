438e24f0a372cc3cf0b617356141cb45acefe99e
vikasyadav5744 <vikram.kumar500@gmail.com>
2025-09-04 11:44:39 +0530

    Update niftyfinal.py

--- niftyfinal.py
+++ niftyfinal.py
@@ -12,13 +12,13 @@ pd.options.mode.copy_on_write = True
 ############################# visualiazation / interpretation of data
 
 tab1, tab2, tab3=st.tabs(["Nothing", "adding data to main file", "Making main file"])
-
-data = st.file_uploader("csv file upload", key='upload1')
-if data!=None:
-  data=pd.DataFrame(data)
-  st.dataframe(data)
-else:
-  st.write("upload file")
+with tab1:
+  data = st.file_uploader("csv file upload", key='upload1')
+  if data!=None:
+    data=pd.DataFrame(data)
+    st.dataframe(data)
+  else:
+    st.write("upload file")
 
 # def all_cal(df):
 #  df=pd.read_csv(data, skiprows=1, usecols=['OI', 'CHNG IN OI', 'VOLUME', 'IV', 'LTP', 'CHNG','BID QTY', 'BID', 'ASK', 'ASK QTY', 'STRIKE', 'BID QTY.1', 'BID.1','ASK.1', 'ASK QTY.1', 'CHNG.1', 'LTP.1','IV.1', 'VOLUME.1','CHNG IN OI.1', 'OI.1',])
@@ -94,6 +94,7 @@ else:
 #             .format(precision=2, subset=['Time', 'CHNG', 'CHNG.1', 'Spot_Price']).format(precision=0, subset=['CALL_LTP', 'PUT_LTP', 'CALL_CHNG','CALL_OI','CALL_VOLUME','PUT_VOLUME','PUT_OI','PUT_CHNG','STRIKE']).map(color_two, subset=['STRIKE']).map(color_all, subset=[ 'Spot_Price','CALL_LTP', 'PUT_LTP','CHNG', 'CHNG.1'])
 #     st.dataframe(love01, hide_index=True, column_order=['Time','CALL_CHNG','CALL_OI','CALL_VOLUME','CALL_LTP','CHNG','STRIKE','PUT_LTP', 'CHNG.1','PUT_VOLUME','PUT_OI','PUT_CHNG','Spot_Price'])
         
+
 
 
 
