0069df49e2f091fe8e99b5d7b759471472de92f0
vikasyadav5744 <vikram.kumar500@gmail.com>
2025-09-07 08:19:28 +0530

    Update niftyfinal.py

--- niftyfinal.py
+++ niftyfinal.py
@@ -29,12 +29,12 @@ with tab1:
     #df['put_per']=(df['PUT_OI']/df['PUT_OI'].max())*100
     #df['call_vol_per']=(df['CALL_VOLUME']/df['CALL_VOLUME'].max())*100
     #df['put_vol_per']=(df['PUT_VOLUME']/df['PUT_VOLUME'].max())*100
-    st.write(df)
+    
     #st.dataframe(df, column_order=['Time','CALL_CHNG','CALL_OI','CALL_VOLUME','CALL_LTP','CHNG','STRIKE','PUT_LTP', 'CHNG.1','PUT_VOLUME','PUT_OI','PUT_CHNG','Spot_Price'])    
   else:
     st.write("upload file")
 
-
+st.write(data)
 
 
 
@@ -112,6 +112,7 @@ with tab1:
 #             .format(precision=2, subset=['Time', 'CHNG', 'CHNG.1', 'Spot_Price']).format(precision=0, subset=['CALL_LTP', 'PUT_LTP', 'CALL_CHNG','CALL_OI','CALL_VOLUME','PUT_VOLUME','PUT_OI','PUT_CHNG','STRIKE']).map(color_two, subset=['STRIKE']).map(color_all, subset=[ 'Spot_Price','CALL_LTP', 'PUT_LTP','CHNG', 'CHNG.1'])
 #     st.dataframe(love01, hide_index=True, column_order=['Time','CALL_CHNG','CALL_OI','CALL_VOLUME','CALL_LTP','CHNG','STRIKE','PUT_LTP', 'CHNG.1','PUT_VOLUME','PUT_OI','PUT_CHNG','Spot_Price'])
         
+
 
 
 
