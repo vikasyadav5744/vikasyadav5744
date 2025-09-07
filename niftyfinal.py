1ad137e3958437f8f9038eb961873f69a68469d0
vikasyadav5744 <vikram.kumar500@gmail.com>
2025-09-07 09:13:37 +0530

    Update niftyfinal.py

--- niftyfinal.py
+++ niftyfinal.py
@@ -33,11 +33,12 @@ with tab1:
       strike1= round1-200
       strike2 = round1+200
       df=df.STRIKE.between(strike1,strike2)
+      st.write(df)
       #st.dataframe(df, column_order=['Time','CALL_CHNG','CALL_OI','CALL_VOLUME','CALL_LTP','CHNG','STRIKE','PUT_LTP', 'CHNG.1','PUT_VOLUME','PUT_OI','PUT_CHNG','Spot_Price'])    
   else:
     st.write("upload file")
 
-st.write(df)
+
 
   
 
@@ -117,6 +118,7 @@ st.write(df)
 #             .format(precision=2, subset=['Time', 'CHNG', 'CHNG.1', 'Spot_Price']).format(precision=0, subset=['CALL_LTP', 'PUT_LTP', 'CALL_CHNG','CALL_OI','CALL_VOLUME','PUT_VOLUME','PUT_OI','PUT_CHNG','STRIKE']).map(color_two, subset=['STRIKE']).map(color_all, subset=[ 'Spot_Price','CALL_LTP', 'PUT_LTP','CHNG', 'CHNG.1'])
 #     st.dataframe(love01, hide_index=True, column_order=['Time','CALL_CHNG','CALL_OI','CALL_VOLUME','CALL_LTP','CHNG','STRIKE','PUT_LTP', 'CHNG.1','PUT_VOLUME','PUT_OI','PUT_CHNG','Spot_Price'])
         
+
 
 
 
