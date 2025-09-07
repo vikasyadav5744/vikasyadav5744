42301cbe599618beda4b5f5c58a1160d463ddc7c
vikasyadav5744 <vikram.kumar500@gmail.com>
2025-09-07 10:12:44 +0530

    Update niftyfinal.py

--- niftyfinal.py
+++ niftyfinal.py
@@ -22,6 +22,19 @@ def highlight_second_highest(s):
       return 'background-color:#f7f4d6; color:black'
   return s.apply(color_val)
 
+# other codes
+def highlight_negative(val):
+  color = 'red' if val < 0 else 'green' 
+  return f'color: {color}'
+
+def color_two(val, props='background-color:orange; color:black'):
+  return props if val >0 else ''
+
+def color_all(val, props='background-color:#f7f4d6; color:black'):
+  return props if val >0 else props
+    
+def color_background_red(val):  
+  return 'background-color:#f7f4d6; color:green' if val > 0 else 'background-color:#f7f4d6; color:red'
 
 
 # visualiazation / interpretation of data
@@ -49,7 +62,8 @@ with tab1:
       strike2 = round1+200
       df=df[df.STRIKE.between(strike1,strike2)]
       df1=df.copy()
-      df1=df1.style.apply(highlight_second_highest,subset=['CALL_OI','PUT_OI','CALL_VOLUME','PUT_VOLUME'])
+      df1=df1.style.apply(highlight_second_highest,subset=['CALL_OI','PUT_OI','CALL_VOLUME','PUT_VOLUME'])\
+      .apply(color_two, subset=['STRIKE'])
       st.dataframe(df1)
       #st.dataframe(df, column_order=['Time','CALL_CHNG','CALL_OI','CALL_VOLUME','CALL_LTP','CHNG','STRIKE','PUT_LTP', 'CHNG.1','PUT_VOLUME','PUT_OI','PUT_CHNG','Spot_Price'])    
   else:
@@ -135,6 +149,7 @@ with tab1:
 #             .format(precision=2, subset=['Time', 'CHNG', 'CHNG.1', 'Spot_Price']).format(precision=0, subset=['CALL_LTP', 'PUT_LTP', 'CALL_CHNG','CALL_OI','CALL_VOLUME','PUT_VOLUME','PUT_OI','PUT_CHNG','STRIKE']).map(color_two, subset=['STRIKE']).map(color_all, subset=[ 'Spot_Price','CALL_LTP', 'PUT_LTP','CHNG', 'CHNG.1'])
 #     st.dataframe(love01, hide_index=True, column_order=['Time','CALL_CHNG','CALL_OI','CALL_VOLUME','CALL_LTP','CHNG','STRIKE','PUT_LTP', 'CHNG.1','PUT_VOLUME','PUT_OI','PUT_CHNG','Spot_Price'])
         
+
 
 
 
