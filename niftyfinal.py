2ebce8010e5855edadcb1d5c01333acd21c9a8e0
vikasyadav5744 <vikram.kumar500@gmail.com>
2025-09-08 12:41:14 +0530

    Update niftyfinal.py

--- niftyfinal.py
+++ niftyfinal.py
@@ -60,12 +60,11 @@ with tab1:
       # bar chart coding
       df2=df.copy()
       option_list=df2.STRIKE.unique()
+      col1, col2=st.columns(2)
       with col1:
         list1=st.selectbox("Select Strike1", options=option_list, index=0, key='list01')
-      with col2:
         list2=st.selectbox("Select Strike2", options=option_list, index=len(option_list)-2, key='list22')
         data_refined=df2[df2.STRIKE.between(list1, list2)]
-      col1, col2=st.columns(2)
       with col1:
         st.bar_chart(data_refined, x='STRIKE', y=['CALL_OI', 'PUT_OI'], color=['#B62626', '#26B669'], stack=False)
       with col2:
@@ -81,6 +80,7 @@ with tab1:
       # com['put_volume']=com['PUT_VOLUME'].astype(str)+'<-->'+'('+com['pvper'].astype(str)+'%)'
       #st.dataframe(com, use_container_width=True, height=500, hide_index=True, column_order=['call_oi','call_volume','STRIKE','put_oi','put_volume']) # column_config={'STRIKE': st.column_config.TextColumn('ID', frozen=True)})
  
+
 
 
 
