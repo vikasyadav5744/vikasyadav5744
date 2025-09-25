efbb4157b01269e6cb7ead10ca69656ab001d720
vikasyadav5744 <vikram.kumar500@gmail.com>
2025-09-25 07:37:07 +0530

    Update niftyfinal.py

--- niftyfinal.py
+++ niftyfinal.py
@@ -63,9 +63,9 @@ with tab1:
       df1=df1.style.apply(highlight_second_highest,subset=['CALL_OI','PUT_OI','CALL_VOLUME','PUT_VOLUME','CALL_CHNG','PUT_CHNG','cvper','pvper']).map(color_two, subset=['STRIKE']).format(precision=0).map(color_all, subset=['ceper','peper','Spot_Price', 'ceprice', 'peprice','PCRval'])
       st.dataframe(df1, width = 1200, height=600, column_order=['Time','ceper','CALL_CHNG','CALL_OI','CALL_VOLUME','cvper','ceprice','STRIKE','peprice','pvper','PUT_VOLUME','PUT_OI','PUT_CHNG','peper','PCRval'])
   
-      check=st.checkbox("get concise view", key="check1")
-      if check==True:
-        st.dataframe(df1, hide_index=True, width = 1200, height=600, column_order=['CALL_CHNG','CALL_OI','CALL_VOLUME','STRIKE','PUT_VOLUME','PUT_OI','PUT_CHNG','cvper', 'pvper','ceper','peper','ceprice','peprice','PCRval'])
+      #check=st.checkbox("get concise view", key="check1")
+      #if check==True:
+        #st.dataframe(df1, hide_index=True, width = 1200, height=600, column_order=['CALL_CHNG','CALL_OI','CALL_VOLUME','STRIKE','PUT_VOLUME','PUT_OI','PUT_CHNG','cvper', 'pvper','ceper','peper','ceprice','peprice','PCRval'])
       
         
       
@@ -91,6 +91,7 @@ with tab1:
     else:
       st.write("upload file")
       
+
 
 
 
