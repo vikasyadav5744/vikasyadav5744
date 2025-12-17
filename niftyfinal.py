c1f89ea18d90ec5ab6d9dfd98e7b9d0c2365a9e1
vikasyadav5744 <vikram.kumar500@gmail.com>
2025-12-17 13:35:19 +0530

    Refactor NIFTY data processing and visualization

--- niftyfinal.py
+++ niftyfinal.py
@@ -4,6 +4,7 @@ import numpy as np
 from datetime import datetime, time
 # change the main file name from here
 st.set_page_config(page_title=None, page_icon=None, layout="wide", initial_sidebar_state="auto", menu_items=None) 
+
 pd.options.mode.copy_on_write = True
 
 # defining functions
@@ -43,7 +44,7 @@ def color_background_red(val):
 # visualiazation / interpretation of data
 
 main_file=[]
-tab1, tab2, tab3=st.tabs(["Nothing", "adding data to main file", "Making main file"])
+tab1, tab2=st.tabs(["Today's NIFTY", "adding data to main file"])
 with tab1:
   data = st.file_uploader("csv file upload", key='upload1')
    
@@ -99,14 +100,18 @@ with tab1:
       with col3:
           st.bar_chart(data_refined, x='STRIKE', y=['CALL_CHNG', 'PUT_CHNG'], color=['#B62626', '#26B669'], stack=False)
 
-if data!=None:
-  st.write(data)
 
-if data!=None:
-    df=pd.read_csv(data, skiprows=1, usecols=['OI', 'CHNG IN OI', 'VOLUME', 'IV', 'LTP', 'CHNG','BID QTY', 'BID', 'ASK', 'ASK QTY', 'STRIKE', 'BID QTY.1', 'BID.1','ASK.1', 'ASK QTY.1', 'CHNG.1', 'LTP.1','IV.1', 'VOLUME.1','CHNG IN OI.1', 'OI.1'])
+
+
+with tab2:
+  data1 = st.file_uploader("csv file upload", key='upload2')
+  if data!=None:
+    spot1=int(st.number_input("Please give spot price", key='spot1', value=26000, step=100))
+    
+    df=pd.read_csv(data1, skiprows=1, usecols=['OI', 'CHNG IN OI', 'VOLUME', 'IV', 'LTP', 'CHNG','BID QTY', 'BID', 'ASK', 'ASK QTY', 'STRIKE', 'BID QTY.1', 'BID.1','ASK.1', 'ASK QTY.1', 'CHNG.1', 'LTP.1','IV.1', 'VOLUME.1','CHNG IN OI.1', 'OI.1'])
     df=df.rename(columns={'OI':'CALL_OI','CHNG IN OI':'CALL_CHNG','VOLUME':'CALL_VOLUME','VOLUME.1':'PUT_VOLUME', 'CHNG IN OI.1':'PUT_CHNG','OI.1':'PUT_OI', 'LTP':'CALL_LTP', 'LTP.1':'PUT_LTP'})
     df=df.replace({",":'', "'":''}, regex=True).replace(r'(?<!^)-', '', regex=True).replace('-',0).astype(float)
-    df['Spot_Price']=spot
+    df['Spot_Price']=spot1
     df['ceper']=(df['CALL_OI']/df['CALL_OI'].max())*100
     df['peper']=(df['PUT_OI']/df['PUT_OI'].max())*100
     df['cvper']=(df['CALL_VOLUME']/df['CALL_VOLUME'].max())*100
@@ -116,9 +121,9 @@ if data!=None:
     df['Sum_CE']=(df['CALL_OI'].sum())
     df['Sum_PE']=(df['PUT_OI'].sum())
     df['Overall_Pcr']=(df['Sum_PE'] / df['Sum_CE'])
-    data=df.copy()   
+     
+st.write()
 
-st.write(data)
 
 
 
