9b070f531f93e2c7ae27e25727f6a339b91ac8a1
vikasyadav5744 <vikram.kumar500@gmail.com>
2025-12-17 12:25:41 +0530

    Update niftyfinal.py

--- niftyfinal.py
+++ niftyfinal.py
@@ -5,6 +5,7 @@ from datetime import datetime, time
 # change the main file name from here
 st.set_page_config(page_title=None, page_icon=None, layout="wide", initial_sidebar_state="auto", menu_items=None) 
 pd.options.mode.copy_on_write = True
+
 # defining functions
 def highlight_second_highest(s):
   max_val = s.max()
@@ -46,12 +47,11 @@ tab1, tab2, tab3=st.tabs(["Nothing", "adding data to main file", "Making main fi
 with tab1:
   data = st.file_uploader("csv file upload", key='upload1')
   #time=st.number_input("Please give time", key='time1')
-  spot=int(st.number_input("Please give spot price", key='spot1'))
+  spot=int(st.number_input("Please give spot price", key='spot1', min_value=26000))
   if data!=None:
     df=pd.read_csv(data, skiprows=1, usecols=['OI', 'CHNG IN OI', 'VOLUME', 'IV', 'LTP', 'CHNG','BID QTY', 'BID', 'ASK', 'ASK QTY', 'STRIKE', 'BID QTY.1', 'BID.1','ASK.1', 'ASK QTY.1', 'CHNG.1', 'LTP.1','IV.1', 'VOLUME.1','CHNG IN OI.1', 'OI.1'])
     df=df.rename(columns={'OI':'CALL_OI','CHNG IN OI':'CALL_CHNG','VOLUME':'CALL_VOLUME','VOLUME.1':'PUT_VOLUME', 'CHNG IN OI.1':'PUT_CHNG','OI.1':'PUT_OI', 'LTP':'CALL_LTP', 'LTP.1':'PUT_LTP'})
     df=df.replace({",":'', "'":''}, regex=True).replace(r'(?<!^)-', '', regex=True).replace('-',0).astype(float)
-    #df['Time']=time
     df['Spot_Price']=spot
     df['ceper']=(df['CALL_OI']/df['CALL_OI'].max())*100
     df['peper']=(df['PUT_OI']/df['PUT_OI'].max())*100
@@ -59,8 +59,10 @@ with tab1:
     df['pvper']=(df['PUT_VOLUME']/df['PUT_VOLUME'].max())*100 
     df['ceprice']= df['STRIKE']+((df['PUT_OI']/df['CALL_OI'])*50)
     df['peprice']= df['STRIKE']-((df['PUT_OI']/df['CALL_OI'])*50)
-    #df['PCRval']=(df['PUT_OI']/['CALL_OI'])*50
+    
+   
     spot1 =df.Spot_Price[0]
+   
     if spot1>0:
       round1 =spot1.round(-2)
       st.write(spot1,round1)
@@ -89,6 +91,7 @@ with tab1:
           st.bar_chart(data_refined, x='STRIKE', y=['CALL_OI', 'PUT_OI'], color=['#B62626', '#26B669'], stack=False)         
       with col3:
           st.bar_chart(data_refined, x='STRIKE', y=['CALL_CHNG', 'PUT_CHNG'], color=['#B62626', '#26B669'], stack=False)
+
 
 
 
